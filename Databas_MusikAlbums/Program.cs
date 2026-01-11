3062bed88c27a0020a9d969837035c7a200eceba
Henry <Henry@DESKTOP-3AO49QU>
2026-01-11 13:01:10 +0100

    Add project files.

---
+++ Databas_MusikAlbums/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Databas_MusikAlbums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
